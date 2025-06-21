b0331d4bc69520cbe82609e70b2922a2fd4d5c7a
ytihianine <ytihianine.pro@gmail.com>
2025-06-22 01:31:36 +0200

    improve bash script to allow more parameters and to specify schema destination

--- import_sqlite.sh
+++ import_sqlite.sh
@@ -2,22 +2,23 @@
 
 SQLITE_PATH="$1"
 PG_URI="$2"
-PGLOADER_SCRIPT_NAME="$3"
+DB_SCHEMA="$3"
+PGLOADER_SCRIPT_NAME="$4"
 
-if [ -z "$SQLITE_PATH" ] || [ -z "$PG_URI" ] || [ -z "$PGLOADER_SCRIPT_NAME" ]; then
-    echo "SQLITE_PATH, PG_URI or PGLOADER_SCRIPT_NAME is empty"
-    echo "Command: $0 <path-to-sqlite> <postgresql-uri> <pgloader-script-name>"
+if [ -z "$SQLITE_PATH" ] || [ -z "$PG_URI" ] || [ -z "$DB_SCHEMA" ] || [ -z "$PGLOADER_SCRIPT_NAME" ]; then
+    echo "SQLITE_PATH, PG_URI, DB_SCHEMA or PGLOADER_SCRIPT_NAME is empty"
+    echo "Command: $0 <path-to-sqlite> <postgresql-uri> <db-schema> <pgloader-script-name>"
     exit 1
 fi
 
 echo "Generating $PGLOADER_SCRIPT_NAME.load file"
-# Generate the .load file
+# Generate the load file
 cat <<EOF > $PGLOADER_SCRIPT_NAME
 LOAD DATABASE
      FROM sqlite:///$SQLITE_PATH
      INTO postgresql://$PG_URI
 CAST type date TO date USING unix-timestamp-to-timestamptz
-
+SET search_path TO '$DB_SCHEMA'
 WITH include drop, create tables, create indexes, reset sequences
 EXCLUDING TABLE NAMES LIKE '_grist%'
 ;
